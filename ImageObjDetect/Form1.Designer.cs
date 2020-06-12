d3be581ea4df422fa19803d896acbdb46bc0a5d6
Khushijani2000 <khushijani18@gnu.ac.in>
2020-06-12 20:02:25 +0530

    Add project files.

---
+++ ImageObjDetect/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ImageObjDetect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picImage
+            // 
+            this.picImage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picImage.Location = new System.Drawing.Point(186, 38);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(483, 299);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picImage.TabIndex = 0;
+            this.picImage.TabStop = false;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(423, 343);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.picImage);
+            this.Name = "Form1";
+            this.Text = "Object Detection";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ImageObjDetect/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace ImageObjDetect
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rect;
+        int x, y, locationx, locationy;
+        public Pen pen = new Pen(Color.White);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form_Load(object sender, EventArgs e)
+        {
+
+
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                picImage.Image = Image.FromFile(ofd.FileName);
+            }
+
+            picImage.MouseDown += new MouseEventHandler(picImage_MouseDown);
+            picImage.MouseMove += new MouseEventHandler(picImage_MouseMove);
+            picImage.MouseEnter += new EventHandler(picImage_MouseEnter);
+            Controls.Add(picImage);
+
+
+        }
+
+
+
+        
+
+            private void picImage_MouseDown(object sender, MouseEventArgs e)
+            {
+            base.OnMouseDown(e);
+                if (e.Button ==System.Windows.Forms.MouseButtons.Left)
+                {
+                Cursor = Cursors.Cross;
+                pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dot;
+                locationx = e.X;
+                locationy = e.Y;
+               
+                
+
+
+
+                }
+            }
+        private void picImage_MouseEnter(object sender, EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Cross;
+        }
+        private void picImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                picImage.Refresh();
+                x = e.X - locationx;
+                y = e.Y - locationx;
+                Graphics g = picImage.CreateGraphics();
+                g.DrawRectangle(pen, locationx, locationy, x, y);
+                g.Dispose();
+
+                label1.Text = "X" + locationx + "\nY" + locationy + "\nWidth" + x + "\nHeight" + y;
+
+
+            }
+
+
+        }
+        
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            Cursor = Cursors.Default;
+        }
+
+
+
+
+
+
+    }
+          
+    }
+
+
